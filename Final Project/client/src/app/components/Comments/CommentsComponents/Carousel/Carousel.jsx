3a247079717cf32b277e2b92b02e7899210bd896
Владислав Маковский <alectorkinga@gmail.com>
2022-06-25 12:08:58 +0300

    update

---
+++ Final Project/client/src/app/components/Comments/CommentsComponents/Carousel/Carousel.jsx
@@ -0,0 +1,76 @@
+// Import:
+import { useState, useEffect } from 'react'
+import { Container, Box } from "@mui/material";
+// ===============================
+// -------------------------------
+// ++++++
+const windowH = "200px";
+const windowW = "80%";
+const itemWidth = "200px";
+const itemHeight = "200px";
+const itemsQuantity = 3;
+const itemGap = "5px";
+
+function CarouselItem({ text }) {
+  return (
+    <Box
+      className="carousel-item"
+      sx={{
+        width: "200px",
+        height: windowH,
+        border: "1px solid red",
+        borderRadius: "10px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        
+      }}
+    >
+      {text}
+    </Box>
+  );
+}
+
+export default function Carousel({ ...props }) {
+  return (
+    <Container
+      className="carousel-container"
+      disableGutters={true}
+      sx={{
+        border: "1px solid red",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.2)",
+      }}
+    >
+      <Box
+        className="carousel-window"
+        sx={{
+          border: "1px solid black",
+          width: "600px",
+          height: itemHeight,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+          backgroundColor: "red",
+        }}
+      >
+        <Box
+          className="carousel-room"
+          sx={{
+            border: "1px solid green",
+            width: "fit-content",
+            height: itemHeight,
+            position: "relative",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        ></Box>
+      </Box>
+    </Container>
+  );
+}
---
+++ Final Project/client/src/app/components/Comments/CommentsStyles.js
@@ -0,0 +1,12 @@
+
+
+export const styles = {
+    "comments-container": {
+        border: "3px solid black",
+        width: "100%", 
+        height: "400px",
+    }
+}
+
+// ======================
+export default styles
